8dda23985ed4d74408977ce6470a0940f06a6eb0
David Riediger <x128@gmx.at>
2019-09-26 20:03:24 +0200

    adds padding to shopcart table

--- shop.js
+++ shop.js
@@ -7,19 +7,24 @@ function ItemConstructor(id, name, price, image) {
 	this.amount = 0;
 }
 
-var item0 = new ItemConstructor(0, 'Basic Item Numero Zorro', 0.01, 'https://via.placeholder.com/200x200.png?text=Item0');
+var item0 = new ItemConstructor(0, 'Basic Item Numero Zorro', 0.00, 'https://via.placeholder.com/200x200.png?text=Item0');
 var item1 = new ItemConstructor(1, 'Fancy Item Numero Uno', 9.90, 'https://via.placeholder.com/200x200.png?text=Item1');
 var item2 = new ItemConstructor(2, 'Super Fancy Item Numero Duo', 19.90, 'https://via.placeholder.com/200x200.png?text=Item2');
 var item3 = new ItemConstructor(3, 'Mega Fancy Item Numero Tres', 29.90, 'https://via.placeholder.com/200x200.png?text=Item3');
 var item4 = new ItemConstructor(4, 'Ultra Fancy Item Numero Quattro', 39.90, 'https://via.placeholder.com/200x200.png?text=Item4');
-var item5 = new ItemConstructor(5, 'Superduper Fancy Item Numero ', 49.90, 'https://via.placeholder.com/200x200.png?text=Item5');
+var item5 = new ItemConstructor(5, 'High Five Machine', 49.90, 'https://via.placeholder.com/200x200.png?text=Item5');
+var item6 = new ItemConstructor(6, 'Sexy Item', 59.90, 'https://via.placeholder.com/200x200.png?text=Item6');
+var item7 = new ItemConstructor(7, 'Lucky Number "you want this"', 70, 'https://via.placeholder.com/200x200.png?text=Item7');
+var item8 = new ItemConstructor(8, 'Too l8 good n8', 80, 'https://via.placeholder.com/200x200.png?text=Item8');
+var item9 = new ItemConstructor(9, 'Buy this when September ends', 90, 'https://via.placeholder.com/200x200.png?text=Item9');
+var item10 = new ItemConstructor(10, "NOW THAT'S AN ITEM", 1000, 'https://via.placeholder.com/200x200.png?text=Item10');
 
-var items = [item0, item1, item2, item3, item4, item5];
+var items = [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10];
 
 // CREATE NEW DIV AND CONTENT FOR EACH ITEM
 function divCreator(item) {
 	var newDiv = document.createElement("div");
-	newDiv.innerHTML = eval(item).image + "<p>" + eval(item).name + "</p><p>€" + eval(item).price + "</p> <input type='button' value='Add to Cart' class='btn'>"
+	newDiv.innerHTML = eval(item).image + "<p>" + eval(item).name + "</p><p>" + eval(item).price + "€</p> <input type='button' value='Add to Cart' class='btn'>"
 	newDiv.className = "item";
 	document.getElementById("product-wrapper").appendChild(newDiv);
 	}
@@ -64,7 +69,7 @@ function Update() {
 		T.innerHTML = "";
 	for (let k = 0; k < cart.length; k++) {
 		T.innerHTML += "<tr><td>" + cart[k].name + "</td><td>" + cart[k].price + 
-		"</td><td>" +
+		"€</td><td>" +
 		"<input type='number' value='" + cart[k].amount + "' onchange='changeAmnt(" + 
 		k +", this.value" + ")'>"
 		+ "</td><td>" +
@@ -72,49 +77,26 @@ function Update() {
 	}
 
 //RAW SUM OF ALL PRODUCTS (prices*amounts)
-	$rawsum = 0;
+	$rawSum = 0;
 	for (let j = 0; j < cart.length; j++) {
-		$rawsum+=cart[j].amount*cart[j].price;
+		$rawSum+=cart[j].amount*cart[j].price;
 	}
 
-	var shipping = ($rawsum < 80) ? 9 : 6;
+	var $totalSum = $rawSum;
 
-//OLD FUNCTION:
-/*	function shippingCost() {
-		var x;
-		if ($rawsum > 80) {
-			x = 6;
-		} else {
-			x = 9;
-		}
-		return(x);
-	}
-	var shipping = shippingCost();*/
-	
-	var tax = $rawsum * 0.22;
-	$rawsum += tax;
-	$rawsum += shipping;
+	var shipping = ($rawSum < 80) ? 9 : 6;
+	var tax = $rawSum * 0.22;
+	$totalSum+=shipping+tax;
 
-	function discount() {
-		var y;
-		if($rawsum < 40) {
-			y = 0;
-		} else if ($rawsum < 100) {
-			y = $rawsum*0.07;
-		} else {
-			y = $rawsum*0.12;
-		}
-		return(y)
-	}
+	var discount = ($totalSum >= 100) ? $totalSum*0.12 : ($totalSum >= 40) ? $totalSum*0.07 : 0;
 
-	disc = discount();
+	$totalSum-=discount;
 	
-	$rawsum -= disc;
-	
-	document.getElementById('shipping').innerHTML = "Shipping Costs: " + shipping + "€";
-	document.getElementById('tax').innerHTML = "Tax (22%): " + tax.toFixed(2) + "€";
-	document.getElementById('disc').innerHTML = "Discount: -" + disc.toFixed(2) + "€";
-	document.getElementById('sum').innerHTML = "<b>Total Cost: " + $rawsum.toFixed(2) + "€</b>";
+	document.getElementById('rawsum').innerHTML = "Raw sum: " + $rawSum.toFixed(2) + "€";
+	document.getElementById('tax').innerHTML = "With tax (22%): " + ($rawSum+tax).toFixed(2) + "€ (+" + tax.toFixed(2) + "€)" ;
+	document.getElementById('shipping').innerHTML = "Shipping: " + shipping + "€";
+	document.getElementById('disc').innerHTML = (discount > 0) ? "Discount: -" + discount.toFixed(2) + "€" : "";
+	document.getElementById('sum').innerHTML = "<b>Total Amount: " + $totalSum.toFixed(2) + "€</b>";
 }
 
 function Del(k) {
